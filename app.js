bd15f8d90e0fde5fb30b188e3896c41d91a44f69
Erica Pisani <pisani.erica@gmail.com>
2018-09-19 16:28:40 -0400

    Small clean up commit.

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const axios = require('axios');
 const app = express()
 const port = 3000
 
+// Create the Axios client and add retry-axios as an Interceptor
+axios.defaults.raxConfig = {
+  noResponseRetries: 4,
+  retry: 4,
+};
+const interceptorId = rax.attach();
+
 app.get('/', async (req, res) => {
   try {
-    const interceptorId = rax.attach();
     const response = await axios({
-      url: 'https://mockbin.org/status/500',
-      raxConfig: {
-        noResponseRetries: 4,
-        retry: 4,
-      }
+      url: 'https://www.mockbin.org/status/500',
+      method: 'get',
     });
   } catch(e) {
-    console.log('Exception thrown', e.config.raxConfig);
+    console.log('Exception thrown', e);
   }
 
-  rax.detach(interceptorId);
   res.send('Hello World!')
 });
 
